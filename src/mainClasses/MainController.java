487453c1c27d4fca92f8d2d371547d64436514ca
forcallinghenney <21600059@handong.edu>
2019-05-14 00:42:53 +0900

    after explain

--- src/mainClasses/MainController.java
+++ src/mainClasses/MainController.java
@@ -8,6 +8,6 @@ public class MainController {
 		 //TODO Auto-generated method stub
 		mainFrame.setThis();
 		mainFrame.add(mainFrame.logPanel);
-		DB.getInstance().addTuples();
+		//DB.getInstance().addTuples();
 	}
 }
