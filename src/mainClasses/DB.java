87c7a27d0dbf64bc075256453b2901ef453ba9d6
forcallinghenney <21600059@handong.edu>
2019-05-16 01:04:39 +0900

    reset

--- src/mainClasses/DB.java
+++ src/mainClasses/DB.java
@@ -14,10 +14,10 @@ public class DB {
 	private String[] artist_names = new String[20];
 
 	private DB() {        	
-        String url = "jdbc:mysql://172.17.192.171/DBengers?serverTimezone=UTC";
+        String url = "jdbc:mysql://172.17.193.38/DBengers?serverTimezone=UTC";
         
         try {
-			con = DriverManager.getConnection(url, "ysh", "thisgood");
+			con = DriverManager.getConnection(url, "kdh", "thisgood");
 		} catch (SQLException e) {
 			System.out.println("connection problem: ");
 			e.printStackTrace();
